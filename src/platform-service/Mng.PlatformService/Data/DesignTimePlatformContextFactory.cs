2199e49ec40fd74e1a0f5e44d7e0e624df227fca
Maksim Sech <maksim.sech@gmail.com>
2022-06-12 01:55:23 +0300

    Add sql server

---
+++ src/platform-service/Mng.PlatformService/Data/DesignTimePlatformContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Mng.PlatformService.Data;
+
+public class DesignTimePlatformContextFactory : IDesignTimeDbContextFactory<PlatformContext>
+{
+    private const string DesignTimeConnectionString = "Server=(localdb)\\mssqllocaldb;Database=PlatformService;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+    public PlatformContext CreateDbContext(string[] args)
+    {
+        var options = new DbContextOptionsBuilder<PlatformContext>()
+            .UseSqlServer(DesignTimeConnectionString).Options;
+
+        return new PlatformContext(options);
+    }
+}
